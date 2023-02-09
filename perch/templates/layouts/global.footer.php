eb37d9792a86f3481443c958bcd337031b655e1e
Jack Barber <jack@natureslaboratory.co.uk>
2023-02-09 09:26:43 +0000

    Update global.footer.php

--- perch/templates/layouts/global.footer.php
+++ perch/templates/layouts/global.footer.php
@@ -7,7 +7,7 @@
 				<strong>WHISH</span></strong><br />
 				&copy; Whitby Hidden Impairments Support and Help <?php echo date('Y'); ?><br />
 				Charity Registration No. 1160390 | <a href="/privacy/">Privacy</a> | <span>01947 899685</span><br /><br />
-				<small><a href="https://natureslaboratory.co.uk">Website Donated by Nature's Laboratory</a></small>
+				<small><a href="https://jackbarber.co.uk" target="_blank">Website Donated by Jack Barber</a></small>
 			</p>
 			<ul>
 				<li></li>
